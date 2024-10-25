ca9d047975272e5f38dec77e1d65eec21c8a8a9e
Muhammad-Usman08 <usman2037966@gmail.com>
2024-10-25 21:53:32 +0500

    app js updated

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ const port = 3000;
 const productData = require('./data/products_data');
 
 
+
 //get method
 app.get('/products', function(req, res){
     res.send(productData);
 });
-
+//get method
+app.get('/', function(req, res){
+    res.send('hello world!');
+});
 app.listen(port, ()=>{
     console.log(`Server is running http://localhost:${port}`);
 })
