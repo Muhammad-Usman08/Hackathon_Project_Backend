45cdfaeb90c500a7b30ff6e255beb5360d487df0
Muhammad-Usman08 <usman2037966@gmail.com>
2024-10-26 11:05:08 +0500

    product updated

--- data/products_data.js
+++ data/products_data.js
@@ -1,79 +1,74 @@
-const products = [
+[
     {
-        category: 'burgers',
-        items: [
+        "category": "burgers",
+        "items": [
             {
-                name: "Zinger",
-                price: 400,
-                description: "Juicy beef patty with cheese, lettuce, and tomato.",
-                image: "https://nasirfoodcenter.shop/wp-content/uploads/2024/08/CLassic-Zinger-Burger.png"
+                "name": "Zinger",
+                "price": 400,
+                "description": "Juicy beef patty with cheese, lettuce, and tomato.",
+                "image": "https://nasirfoodcenter.shop/wp-content/uploads/2024/08/CLassic-Zinger-Burger.png"
             },
             {
-                name: "Beef Burger",
-                price: 350,
-                description: "Beef patty topped with crispy bacon and cheddar cheese.",
-                image: "https://this.co/wp-content/uploads/2022/09/Beef-Burger-768x768.jpg"
-
+                "name": "Beef Burger",
+                "price": 350,
+                "description": "Beef patty topped with crispy bacon and cheddar cheese.",
+                "image": "https://this.co/wp-content/uploads/2022/09/Beef-Burger-768x768.jpg"
             },
             {
-                name: "Zinger with Cheese",
-                price: 450,
-                description: "A healthy veggie patty with fresh lettuce and tomato.",
-                image: "https://mirchihut.pk/wp-content/uploads/2020/09/zinger-cheese-burger.jpg"
-
+                "name": "Zinger with Cheese",
+                "price": 450,
+                "description": "A healthy veggie patty with fresh lettuce and tomato.",
+                "image": "https://mirchihut.pk/wp-content/uploads/2020/09/zinger-cheese-burger.jpg"
             }
-        ],
+        ]
     },
     {
-        category: 'desserts',
-        items: [
+        "category": "desserts",
+        "items": [
             {
-                name: "Chocolate Cake",
-                price: 600,
-                description: "Rich chocolate cake with creamy frosting.",
-                image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlrBaBRcJWnJK3PG4uNJAASWU9HNj7BdBnhA&s"
+                "name": "Chocolate Cake",
+                "price": 600,
+                "description": "Rich chocolate cake with creamy frosting.",
+                "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlrBaBRcJWnJK3PG4uNJAASWU9HNj7BdBnhA&s"
             },
             {
-                name: "Ice Cream Sundae",
-                price: 450,
-                description: "Vanilla ice cream with chocolate syrup and nuts.",
-                image: "https://www.keep-calm-and-eat-ice-cream.com/wp-content/uploads/2022/08/Ice-cream-sundae-hero-11.jpg"
-
+                "name": "Ice Cream Sundae",
+                "price": 450,
+                "description": "Vanilla ice cream with chocolate syrup and nuts.",
+                "image": "https://www.keep-calm-and-eat-ice-cream.com/wp-content/uploads/2022/08/Ice-cream-sundae-hero-11.jpg"
             },
             {
-                name: "Apple Pie",
-                price: 800,
-                description: "Classic apple pie with a flaky crust.",
-                image: "https://herbsandflour.com/wp-content/uploads/2020/10/Classic-Apple-Pie-791x1024.jpg"
-
+                "name": "Apple Pie",
+                "price": 800,
+                "description": "Classic apple pie with a flaky crust.",
+                "image": "https://herbsandflour.com/wp-content/uploads/2020/10/Classic-Apple-Pie-791x1024.jpg"
             }
-        ],
+        ]
     },
     {
-        category: 'chinese',
-        items: [
+        "category": "chinese",
+        "items": [
             {
-                name: "Nuddles",
-                price: 600,
-                description: "Hot and Spicy Nuddles with egg",
-                image: "https://s3-alpha-sig.figma.com/img/3506/0211/0447761fa98ff6a68e27e28dfc5ececf?Expires=1730678400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=m1NAm8xA6m3vVFP-Kmkaw-eXs5op-liPsCis1mk3agTe~JWcIWtkqcVPDt09G4tFQlL8UEVHxtOjiPq8ONMeHpO5wOabsDopMhhjfifO-aU10h4n0SaeHal0KoHVy1loNntWN1kdP~6VzO9ZqpDyKINAbNsEldPtx7ARE83PsUPEc-5yd8vFJ~KAxnDXoZjpzGEOUH6GiGEmEwbzk1l4iEnuTrnVhoeIfoiAeuD0VT3QAccQVXpHgTDDaIZpjnSNLD9fRmzsPXdLTCFNp9WgQcxneFrWMHbwsLknGskJLoOi-gvsYh3~J~RzeEzTx3WAIiJKaGU~2eB2vZDWzy8nKg__"
+                "name": "Noodles",
+                "price": 600,
+                "description": "Hot and Spicy Noodles with egg",
+                "image": "https://s3-alpha-sig.figma.com/img/3506/0211/0447761fa98ff6a68e27e28dfc5ececf?Expires=1730678400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=m1NAm8xA6m3vVFP-Kmkaw-eXs5op-liPsCis1mk3agTe~JWcIWtkqcVPDt09G4tFQlL8UEVHxtOjiPq8ONMeHpO5wOabsDopMhhjfifO-aU10h4n0SaeHal0KoHVy1loNntWN1kdP~6VzO9ZqpDyKINAbNsEldPtx7ARE83PsUPEc-5yd8vFJ~KAxnDXoZjpzGEOUH6GiGEmEwbzk1l4iEnuTrnVhoeIfoiAeuD0VT3QAccQVXpHgTDDaIZpjnSNLD9fRmzsPXdLTCFNp9WgQcxneFrWMHbwsLknGskJLoOi-gvsYh3~J~RzeEzTx3WAIiJKaGU~2eB2vZDWzy8nKg__"
             },
             {
-                name: "French Fries",
-                price: 200,
-                description: "Crispy,Hot and Spicy Fries With Dip",
-                image: "https://s3-alpha-sig.figma.com/img/bf91/ddf7/41465d7ceae68bb4bb9c09375cef3ac9?Expires=1730678400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=PjyWUvM0GN6~fQUPkVuVvmT65o3-6oLm~vzEv~ZXS9J037a8-C8Ipz3j~1lCvdMRFhGC69nuE4d0Bw1-vRG8ssZAhKyT2WM7V8vYl-8gfa54vtOmG59-MXYNOI33TcaX36a8PGkkJ~bQ2bNZY2LNlgxnUHSIMsw21qyE18QRnytuBHf5wrBgnZJKaC-sUzYdX-q~ie8dYjH0zO~13SDvXqDaZWxETTPQf6oQl1yevxT~15PYhkDBUJcZDe55VoPqfsHS2v2N5OdMuMbyjNmUWD2RUY~IFls09-jDLywyz85qg1EFpla3SOLU2bBJ2-df1QLullLFEPj6RFkTSCoHdA__"
-
+                "name": "French Fries",
+                "price": 200,
+                "description": "Crispy, Hot and Spicy Fries With Dip",
+                "image": "https://s3-alpha-sig.figma.com/img/bf91/ddf7/41465d7ceae68bb4bb9c09375cef3ac9?Expires=1730678400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=PjyWUvM0GN6~fQUPkVuVvmT65o3-6oLm~vzEv~ZXS9J037a8-C8Ipz3j~1lCvdMRFhGC69nuE4d0Bw1-vRG8ssZAhKyT2WM7V8vYl-8gfa54vtOmG59-MXYNOI33TcaX36a8PGkkJ~bQ2bNZY2LNlgxnUHSIMsw21qyE18QRnytuBHf5wrBgnZJKaC-sUzYdX-q~ie8dYjH0zO~13SDvXqDaZWxETTPQf6oQl1yevxT~15PYhkDBUJcZDe55VoPqfsHS2v2N5OdMuMbyjNmUWD2RUY~IFls09-jDLywyz85qg1EFpla3SOLU2bBJ2-df1QLullLFEPj6RFkTSCoHdA__"
             },
             {
-                name: "Chicken Vegge Salad",
-                price: 300,
-                description: "Salad with lot of vegetables and cheese and cucumber",
-                image: "https://s3-alpha-sig.figma.com/img/ee1c/7165/d1d152424464e5559fc4281ce81ea410?Expires=1730678400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=cJxNrGMuVc-7X3pRS28~KKbB1eNu8kItrYwUuyxv7rbcgXwkkPXzI8mbcemXFaebxJnoUsXLbX0s0ISpqTlVKThPxqjUHfL8zzPyiTPeUk89Ro8kyL4cLtdLYLSDAz7q7azPHwwXDJqQlkJ1Uax3F2SjNv2k8M8Dnz0O0h2n1z3z~wjkAtunDcapLmv9gSuYAaKVNaT86MuFyBcyqjrx-X6X52LPLhdmVGBLuhXHoMVdisMhkYZP45z949P53JEJLlGZe22dX-cuGqgFWH7CLXwFjhaZZJfPeskCpggJzDPi7w4~86JZQoRGtim-mnCKwi0vhDj27t0WoO2bVTDFhg__"
-
+                "name": "Chicken Veggie Salad",
+                "price": 300,
+                "description": "Salad with a lot of vegetables, cheese, and cucumber",
+                "image": "https://s3-alpha-sig.figma.com/img/ee1c/7165/d1d152424464e5559fc4281ce81ea410?Expires=1730678400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=cJxNrGMuVc-7X3pRS28~KKbB1eNu8kItrYwUuyxv7rbcgXwkkPXzI8mbcemXFaebxJnoUsXLbX0s0ISpqTlVKThPxqjUHfL8zzPyiTPeUk89Ro8kyL4cLtdLYLSDAz7q7azPHwwXDJqQlkJ1Uax3F2SjNv2k8M8Dnz0O0h2n1z3z~wjkAtunDcapLmv9gSuYAaKVNaT86MuFyBcyqjrx-X6X52LPLhdmVGBLuhXHoMVdisMhkYZP45z949P53JEJLlGZe22dX-cuGqgFWH7CLXwFjhaZZJfPeskCpggJzDPi7w4~86JZQoRGtim-mnCKwi0vhDj27t0WoO2bVTDFhg__"
             }
-        ],
-    },
-];
+        ]
+    }
+]
+
 
 module.exports = products;
